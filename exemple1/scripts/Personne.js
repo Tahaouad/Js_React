ba84e31f4e0ad5c474c076cf5a395f4e91aa9d1b
Tahaouad <tahaouad04@gmail.com>
2023-11-13 01:38:12 +0100

    Revition import export heritage

---
+++ exemple1/scripts/Personne.js
@@ -0,0 +1,8 @@
+import personne from "./script.js" ;
+import { S1,afficher } from "./script.js" ;
+
+let P = new personne("Ahmad","TEST")
+console.log() ;
+document.getElementById('info').innerHTML = afficher(S1)
+
+console.log(P.nom)
---
+++ exemple1/scripts/script.js
@@ -0,0 +1,20 @@
+export default class personne {
+    constructor(Nom,Prenom){
+        this.nom = Nom ;
+        this.prenom = Prenom ;
+
+    }
+}
+export function afficher(object){
+    return ` NOM : ${object.nom} | Prenom : ${object.prenom} | Filiere : ${object.filiere}`
+}
+
+class Stagaire extends personne  {
+    constructor(nom,prenom,filiere){
+        super(nom,prenom)
+        this.filiere = filiere
+    }
+}
+let E1 = new personne('Ouad','Taha')
+
+export let S1 =new Stagaire(E1.nom,E1.prenom,'DD102')
