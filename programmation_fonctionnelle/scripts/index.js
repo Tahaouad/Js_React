fc4668d2d03253f81ae63c6f6d88c6e7cfe1ab64
Tahaouad <tahaouad04@gmail.com>
2023-11-15 22:26:38 +0100

    Exercice programmation fonctionnel

--- programmation_fonctionnelle/scripts/index.js
+++ programmation_fonctionnelle/scripts/index.js
@@ -1,42 +1,44 @@
 const h = document.getElementById('hours');
 const m = document.getElementById('minutes');
 const s = document.getElementById('seconds');
+let id; // Ajout de la déclaration de la variable id
 
 let initTime = { h: 11, m: 59, s: 52 };
 
 function showTime(time) {
-    h.innerHTML = time.h<10?"0"+time.h:time.h;
-    m.innerHTML = time.m<10?"0"+time.m:time.m;
-    s.innerHTML = time.s<10?"0"+time.s:time.s;
+    h.innerHTML = time.h < 10 ? "0" + time.h : time.h;
+    m.innerHTML = time.m < 10 ? "0" + time.m : time.m;
+    s.innerHTML = time.s < 10 ? "0" + time.s : time.s;
 }
 
 function increment(time) {
     let myTime = { ...time };
     return function () {
         myTime.s++;
-        if(myTime.s === 60){
-            myTime.s = 0
-            myTime.m++
-            if(myTime.m === 60){
-                myTime.m = 0 
-                myTime.h++ 
+        if (myTime.s === 60) {
+            myTime.s = 0;
+            myTime.m++;
+            if (myTime.m === 60) {
+                myTime.m = 0;
+                myTime.h++;
+                if (myTime.h === 12) myTime.h = 0;
             }
-            if(myTime.h === 12 ) myTime.h=0
         }
-        showTime(myTime)
+        showTime(myTime);
     };
 }
-function stop() {
-    if(initTime){
-    clearInterval(id);
-    }
-}
-function start() {
-    let id = setInterval(incrementTime,1000)
 
+
+
+function stop() {
+    clearInterval(id);
+}
+
+function start() {
+
+    id = setInterval(incrementTime, 1000); 
 }
 
 const incrementTime = increment(initTime);
 incrementTime();
-let id = setInterval(incrementTime,1000)
-
+start(); 
